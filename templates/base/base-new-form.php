145df93c6911785b47baf0a00e3eaab355683a48
Takao Kawasaki <tek.tekeo.lab@gmail.com>
2021-02-08 14:08:02 +0900

    親要素の登録処理をクラス化

--- templates/base/base-new-form.php
+++ templates/base/base-new-form.php
@@ -1,130 +1,79 @@
 <?php //テンプレートフォーム
 use AjaxSnippets\Common\CreateForm as CF;
-use AjaxSnippets\Domain\Models\Dog;
+use AjaxSnippets\Domain\Models\BaseModel;
+use AjaxSnippets\Domain\Models\Base;
+use AjaxSnippets\Domain\Models\Detail;
+use AjaxSnippets\Domain\Models\Apps;
 
 if ( !defined( 'ABSPATH' ) ) exit; ?>
 
-  <?php
-
-
+<?php
+  //新規登録処理
   $check_base_insert=$_POST['base_insert'];
   if ($check_base_insert != ""){
-    //TODO: 送信された場合の処理
-    // BM::addNewAnken($_POST);
-  }
-  global $wpdb;
-  $name = $_POST['name']; //表示する名前　必須
-  $anken = $_POST['anken']; //URLのケツにつける案件コード　必須
-  $affi_link = $_POST['affi_link']; //アフィリエイトリンク　必須
-  $s_link = $_POST['s_link']; //商品リンク　任意
-  $asp_name = $_POST['asp_name']; //aspの名前 必須
-  $affi_img = $_POST['affi_img']; //アフィリエイトの画像
-  $img_tag = $_POST['img_tag']; //計測用のタグ
-  $official_link = $_POST['official_link']; //公式リンク　必須
 
-  if($name != '' && $anken != '' && $affi_link != '' && $asp_name != '' && $official_link !=''){
-    $name = $_POST['name'];
-    $anken = $_POST['anken'];
-    $affi_link = $_POST['affi_link'];
-    $s_link = $_POST['s_link'];
-    $asp_name = $_POST['asp_name'];
-    $affi_img = $_POST['affi_img'];
-    $img_tag = $_POST['img_tag'];
-    $official_link = $_POST['official_link'];
-    $img = $_POST['img'];
-    $dev = $_POST['dev'];
-    $ios_link = $_POST['ios_link'];
-    $android_link = $_POST['android_link'];
-    $web_link = $_POST['web_link'];
-    $ios_affi_link = $_POST['ios_affi_link'];
-    $android_affi_link = $_POST['android_affi_link'];
-    $web_affi_link = $_POST['web_affi_link'];
-    $article = $_POST['article'];
-    $app_order = $_POST['app_order'];
-    $app_price = $_POST['app_price'];
+    $base_insert = new Base();
+    $base_id = $base_insert->exeInsert($_POST);
 
+    $_POST['base_id'] = $base_id;
+    $_POST['item_name'] = "トップ";
+    $_POST['affi_item_link'] = $_POST['affi_link'];
+    $_POST['official_item_link'] = $_POST['official_link'];
 
+    $detail_insert = new Detail();
+    $detail_id = $detail_insert->exeInsert($_POST);
 
-    $table = PLUGIN_DB_PREFIX.'base';
+    $_POST['app_id'] = $base_id;
+    $app_insert = new Apps();
+    $app_id = $app_insert->exeInsert($_POST);
 
-    $data = array('name'=>$name, 'anken'=>$anken, 'affi_img' => $affi_img, 'affi_link' => $affi_link, 's_link'=>$s_link, 'asp_name'=>$asp_name,'img_tag' => $img_tag);
-    //$format = array('%d','%d','%d','%s','%f');
-
-    $res = $wpdb->insert( $table, $data );
-    if($res){
-        $base_id = $wpdb->insert_id;
-        $table = PLUGIN_DB_PREFIX.'detail';
-        $affi_item_link =$affi_link;//トップはアフィリンク共通
-        $data = array('id'=>'','base_id'=>$base_id,'item_name'=>'トップ','official_item_link'=>$official_link,'affi_item_link'=>$affi_item_link,'amazon_asin'=>'','rakuten_id'=>'');
-
-        $res1 = $wpdb->insert( $table, $data);
-        if($res1){
-          $table = PLUGIN_DB_PREFIX.'apps';
-          if($app_price=''){$app_price = '無料';}
-          if($app_order=''){$app_order = 0;}
-          $data = array('app_id'=>$base_id, 'img'=>$img, 'dev' => $dev, 'ios_link' => $ios_link, 'android_link'=>$android_link, 'web_link'=>$web_link,'ios_affi_link' => $ios_affi_link, 'android_affi_link'=>$android_affi_link, 'web_affi_link'=>$web_affi_link,'article' => $article, 'app_order' => $app_order, 'app_price' => $app_price);
-
-          $res = $wpdb->insert( $table, $data );
-
-          echo "<p style='color:red'>DETAILトップの登録できました</p>";
-          $name ="";
-          $anken ="";
-          $affi_link ="";
-          $s_link="";
-          $asp_name="a8";
-          $affi_img ="";
-          $img_tag ="";
-          $official_link ="";
-          $info ="";
-          $review ="";
-          $rchart ="";
-        }else{
-          echo "<p style='color:red'>個別だけミスったぽい</p>";
-        }
-      echo "<h1 class='red'>BASEの入力完了！</h1>";
+    if($base_id && $detail_id && $app_id){
+      $attention_comment = "新規挿入が完了！！！！！";
+      unset($_POST);
+    }else if($base_id){
+      $attention_comment = "小要素だけミスった";
     }else{
-      echo "<h1 class='red'>変更なし</h1>";
+      $attention_comment = "親、子、共にミスってる";
     }
-  }else{
-    echo "<h1 class='red'>入力不備</h1>";
   }
   ?>
-
+<p>
 <a href="<?php echo admin_url('')."admin.php?page=ajax-snippets&action=edit"; ?>" style="font-size:20px;">編集ページへ</a>
 <a href="<?php echo admin_url('')."admin.php?page=ajax-snippets&action=delete"; ?>" style="font-size:20px;">削除ページへ</a>
-<p style="font-size:20px"><?=$attention_comment?></p>
+</p>
+<p style="font-size:20px; color:red"><?=$attention_comment?></p>
 <form name="form1" method="post" action="">
   <table class="input_column2_table">
     <tbody><caption>親要素(base)の新規追加フォーム</caption>
       <tr>
-      <th>名前</th>                     <td><?=CF::textBox('name', $name, 'required')?></td>
+      <th>名前 必須</th>                  <td><?=CF::textBox('name', $_POST['name'], 'required')?></td>
       </tr>
       <tr>
-      <th>案件コード</th>                <td><?=CF::textBox('anken', $anken, 'required')?></td>
+      <th>案件コード 必須</th>             <td><?=CF::textBox('anken', $_POST['anken'], 'required')?></td>
       </tr>
       <tr>
-      <th>アフィリンク（メイン）</th>      <td><?=CF::textBox('affi_link',$affi_link, 'required')?></td>
+      <th>アフィリンク（メイン）必須</th>    <td><?=CF::textBox('affi_link',$_POST['affi_link'], 'required')?></td>
       </tr>
       <tr>
-      <th>アフィリンク（商品リンクの頭）</th><td><?=CF::textBox('s_link', $s_link)?></td>
+      <th>アフィリンク(商品リンクの頭)</th>  <td><?=CF::textBox('s_link', $_POST['s_link'])?></td>
       </tr>
       <tr>
-      <th>提携ASP</th>                   <td><?=CF::sqlSelectBox(PLUGIN_DB_PREFIX."asp", 'asp_name', array('asp_name','asp_name'),'', 'required')?></td>
+      <th>提携ASP　必須</th>              <td><?=CF::sqlSelectBox(PLUGIN_DB_PREFIX."asp", 'asp_name', array('asp_name','asp_name'),$_POST['asp_name'], 'required')?></td>
       </tr>
       <tr>
-      <th>バナー画像</th>                 <td><?=CF::textBox('affi_img', $affi_img)?></td>
+      <th>バナー画像</th>                 <td><?=CF::textBox('affi_img', $_POST['affi_img'])?></td>
       </tr>
       <tr>
-      <th>バナーの幅</th>                 <td><?=CF::numBox('img_width', $img_width, '300')?></td>
+      <th>バナーの幅</th>                 <td><?=CF::numBox('img_width', $_POST['img_width'], '300')?></td>
       </tr>
       <tr>
-      <th>バナーの高さ</th>               <td><?=CF::numBox('img_height', $img_width,'250')?></td>
+      <th>バナーの高さ</th>               <td><?=CF::numBox('img_height', $_POST['img_width'],'250')?></td>
       </tr>
       <tr>
-      <th>アフィ、トラッキングイメージタグ</th><td><?=CF::textBox('img_tag', $img_tag)?></td>
+      <th>アフィ、トラッキングイメージタグ</th><td><?=CF::textBox('img_tag', $_POST['img_tag'])?></td>
       </tr>
       <tr>
-      <th>公式サイトURL</th>               <td><?=CF::textBox('official_link', $official_link, 'required')?></td>
+      <th>公式サイトURL　必須</th>               <td><?=CF::textBox('official_link', $_POST['official_link'], 'required')?></td>
       </tr>
       <tr>
       <th colspan=2><input type="submit" value="新規挿入" name="base_insert" style="width:100%;padding:30px"></th>
@@ -133,37 +82,37 @@ if ( !defined( 'ABSPATH' ) ) exit; ?>
       <th colspan=2>↓以下、アプリの情報がある場合のみ</th>
       </tr>
       <tr>
-      <th>アプリのアイコン画像URL</th><td><?=CF::imgUploadBox($img)?></td>
+      <th>アプリのアイコン画像URL</th>      <td><?=CF::imgUploadBox($_POST['img'])?></td>
       </tr>
       <tr>
-      <th>開発企業</th><td><?=CF::textBox('dev', $dev)?></td>
+      <th>開発企業</th>                   <td><?=CF::textBox('dev', $_POST['dev'])?></td>
       </tr>
       <tr>
-      <th>iosのリンク先</th><td><?=CF::textBox('ios_link', $ios_link)?></td>
+      <th>iosのリンク先</th>              <td><?=CF::textBox('ios_link', $_POST['ios_link'])?></td>
       </tr>
       <tr>
-      <th>androidのリンク先</th><td><?=CF::textBox('android_link', $android_link)?></td>
+      <th>androidのリンク先</th>          <td><?=CF::textBox('android_link', $_POST['android_link'])?></td>
       </tr>
       <tr>
-      <th>webのリンク先</th><td><?=CF::textBox('web_link', $web_link)?></td>
+      <th>webのリンク先</th>               <td><?=CF::textBox('web_link', $_POST['web_link'])?></td>
       </tr>
       <tr>
-      <th>iosのアフィリンク先</th><td><?=CF::textBox('ios_affi_link', $ios_affi_link)?></td>
+      <th>iosのアフィリンク先</th>          <td><?=CF::textBox('ios_affi_link', $_POST['ios_affi_link'])?></td>
       </tr>
       <tr>
-      <th>androidのアフィリンク先</th><td><?=CF::textBox('android_affi_link', $android_affi_link)?></td>
+      <th>androidのアフィリンク先</th>      <td><?=CF::textBox('android_affi_link', $_POST['android_affi_link'])?></td>
       </tr>
       <tr>
-      <th>webのアフィリンク先</th><td><?=CF::textBox('web_affi_link', $web_affi_link)?></td>
+      <th>webのアフィリンク先</th>          <td><?=CF::textBox('web_affi_link', $_POST['web_affi_link'])?></td>
       </tr>
       <tr>
-      <th>レビュー記事のURL</th><td><?=CF::textBox('article', $article)?></td>
+      <th>レビュー記事のURL</th>            <td><?=CF::textBox('article', $_POST['article'])?></td>
       </tr>
       <tr>
-      <th>アプリの表示順</th><td><?=CF::numBox('app_order', $app_order)?></td>
+      <th>アプリの表示順</th>               <td><?=CF::numBox('app_order', $_POST['app_order'])?></td>
       </tr>
       <tr>
-      <th>アプリの料金</th><td><?=CF::textBox('app_price', $app_price)?></td>
+      <th>アプリの料金</th>                 <td><?=CF::textBox('app_price', $_POST['app_price'])?></td>
       </tr>
     </tbody>
   </table>
