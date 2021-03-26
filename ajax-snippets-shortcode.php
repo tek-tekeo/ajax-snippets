65cc791a6550a88f3f99ffe68fd21ab330d2145a
Takao Kawasaki <tek.tekeo.lab@gmail.com>
2021-03-26 16:24:54 +0900

    DMM修正

--- ajax-snippets-shortcode.php
+++ ajax-snippets-shortcode.php
@@ -8,7 +8,7 @@ function AjaxSniShortcodeLink($atts, $content = null) {
         're_url'=>'0'
     ), $atts ) );
     global $wpdb;
-    $sql = "SELECT B.name, B.asp_name, B.affi_link, B.affi_img, B.img_tag, D.id, D.item_name,D.affi_item_link D.official_item_link FROM ".PLUGIN_DB_PREFIX."base As B RIGHT JOIN ".PLUGIN_DB_PREFIX."detail As D ON B.id = D.base_id where D.id={$id}";
+    $sql = "SELECT B.name, B.asp_name, B.affi_link, B.affi_img, B.img_tag, D.id, D.item_name,D.affi_item_link, D.official_item_link FROM ".PLUGIN_DB_PREFIX."base As B RIGHT JOIN ".PLUGIN_DB_PREFIX."detail As D ON B.id = D.base_id where D.id={$id}";
 
     $results = $wpdb->get_results($sql,object);
         if(count($results) == 0){
