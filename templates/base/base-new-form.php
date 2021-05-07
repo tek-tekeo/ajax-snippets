25304cf4eb1f024cd87da44e2422094d8a23dd99
Takao Kawasaki <tek.tekeo.lab@gmail.com>
2021-05-07 15:52:29 +0900

    親要素追加時の小要素は、親と同じ場合にチェックが入る

--- templates/base/base-new-form.php
+++ templates/base/base-new-form.php
@@ -17,6 +17,7 @@ if ( !defined( 'ABSPATH' ) ) exit; ?>
 
     $_POST['base_id'] = $base_id;
     $_POST['item_name'] = "000";
+    $_POST['same_parent'] = 1;
     $_POST['affi_item_link'] = $_POST['affi_link'];
     $_POST['official_item_link'] = $_POST['official_link'];
 
