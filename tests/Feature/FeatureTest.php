29a84ab34651765188763fc9e7173091cecda444
tek-tekeo <tek.tekeo.lab@gmail.com>
2023-11-29 18:13:20 +0900

    fix: テスト環境の修正

--- tests/Feature/FeatureTest.php
+++ tests/Feature/FeatureTest.php
@@ -1,7 +1,7 @@
 <?php declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 
-final class ExampleTest extends TestCase
+final class FeatureTest extends TestCase
 {
     public function testExample(): void
     {
---
+++ tests/Unit/SampleTest.php
@@ -0,0 +1,10 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+final class SampleTest extends TestCase
+{
+    public function testExample(): void
+    {
+        $this->assertSame(true, true);
+    }
+}
