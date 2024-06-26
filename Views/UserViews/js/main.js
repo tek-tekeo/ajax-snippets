d7054a859a94fdee8679bd115bb2698df81b178d
tek-tekeo <tek.tekeo.lab@gmail.com>
2024-06-26 10:31:16 +0900

    add: vuetifyを適用する

--- Views/UserViews/js/main.js
+++ Views/UserViews/js/main.js
@@ -20,7 +20,7 @@ axios.defaults.baseURL = WP_API_Settings.root+'?rest_route=/ajax_snippets_path/v
 
   new Vue({
     el:'#content-in', // 範囲を広すぎると、他のjavascriptに干渉してしまう　　　HTMLElementをそのままelプロパティに渡す　
-    // vuetify: new Vuetify(),
+    vuetify: new Vuetify(),
     components: {
       // 'affiliateLink': httpVueLoader('/wp-content/plugins/ajax-snippets/Views/UserViews/js/affiliateLink.vue'),
       // 'rakutenBannerLink': httpVueLoader('/wp-content/plugins/ajax-snippets/Views/UserViews/js/rakutenBannerLink.vue'),
@@ -28,6 +28,7 @@ axios.defaults.baseURL = WP_API_Settings.root+'?rest_route=/ajax_snippets_path/v
       // 'appLink': httpVueLoader('/wp-content/plugins/ajax-snippets/Views/UserViews/js/appLink.vue'),
       'chartJs': httpVueLoader('/wp-content/plugins/ajax-snippets/Views/UserViews/js/chartJs.vue'),
       'clickLog': httpVueLoader('/wp-content/plugins/ajax-snippets/Views/UserViews/js/clickLog.vue'),
+      'reviews': httpVueLoader('/wp-content/plugins/ajax-snippets/Views/UserViews/js/reviews.vue')
     },
     methods:{
       async clickRecord(logData) {
