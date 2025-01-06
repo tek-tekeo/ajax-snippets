974e32cadd52c1d655b514b9d6b8171f5a6ad2c2
tek-tekeo <tek.tekeo.lab@gmail.com>
2025-01-06 19:10:30 +0900

    fix: 楽天リンクページでの更新ボタンでリストから削除する

--- Views/AdminViews/pages/rakutenLinkPage.vue
+++ Views/AdminViews/pages/rakutenLinkPage.vue
@@ -10,9 +10,8 @@
     </v-row>
     <v-row>
       <v-col cols="1">ID</v-col>
-      <v-col cols="1">状態</v-col>
       <v-col cols="1">掲載数</v-col>
-      <v-col cols="1">日時</v-col>
+      <v-col cols="2">日時</v-col>
       <v-col cols="1">画像</v-col>
       <v-col cols="4">商品名</v-col>
       <v-col cols="2">楽天商品ID</v-col>
@@ -20,16 +19,13 @@
     </v-row>
     <v-row v-for="l in rakutenLinks" :key="'rakuten-link-' + l.id">
       <v-col cols="1"><router-link :to="'/detail/update/' + l.id" target="_blank">{{ l.id }}</router-link></v-col>
-      <v-col cols=" 1">
-        <v-checkbox v-model="l.isLinkActive" disabled color="blue"></v-checkbox>
-      </v-col>
       <v-col cols="1">
         <a
           :href="`/wp-admin/edit.php?s=%5Brakuten2+id%3D%22` + l.rakutenId + `&post_status=all&post_type=post&action=-1&m=0&cat=0&author=0&paged=1&action2=-1`">
           一覧
         </a>
       </v-col>
-      <v-col cols="1">{{ l.rakutenExpiredAt }}</v-col>
+      <v-col cols="2">{{ l.rakutenExpiredAt }}</v-col>
       <v-col cols="1"><img :src="l.imageUrl" width=100 height=100 /></v-col>
       <v-col cols="4">
         <wp-text-box v-model="l.itemName" readonly>
@@ -46,7 +42,7 @@
         </wp-text-box>
       </v-col>
       <v-col cols="1">
-        <v-btn v-show="!l.isLinkActive" @click="rakutenLinkUpdate(l)" dark color="teal">
+        <v-btn @click="rakutenLinkUpdate(l)" dark color="teal">
           更新
         </v-btn>
         <confirm-dialog v-show="!(l.deletedAt)" @execute="deleteAdDetail(l)">
@@ -77,10 +73,7 @@ module.exports = {
     async hasDeletedAt() {
       if (this.hasDeletedAt) {
         const res = await axios.get('detail/deletedItems');
-        this.rakutenLinks = res.data.map((l) => {
-          l.isLinkActive = false;
-          return l;
-        });
+        this.rakutenLinks = res.data
       } else {
         await this.getRakutenLinks();
       }
@@ -91,10 +84,7 @@ module.exports = {
       const res = await axios.get('detail/' + obj.id + '/restore');
       if (res.data) {
         const res = await axios.get('detail/deletedItems');
-        this.rakutenLinks = res.data.map((l) => {
-          l.isLinkActive = false;
-          return l;
-        });
+        this.rakutenLinks = res.data
         const options = {
           position: 'bottom-right',
           duration: 2000,
@@ -121,10 +111,7 @@ module.exports = {
     },
     async getRakutenLinks() {
       const res = await axios.post('detail/rakutenLinkExpired', { hasDeletedAt: this.hasDeletedAt });
-      this.rakutenLinks = res.data.map((l) => {
-        l.isLinkActive = false;
-        return l;
-      });
+      this.rakutenLinks = res.data;
     },
     async rakutenLinkUpdate(obj) {
       const res = await axios.put('detail/rakutenLinkUpdate', obj);
@@ -134,7 +121,8 @@ module.exports = {
         fullWidth: false
       }
       if (res.data.success) {
-        obj.isLinkActive = true;
+        await this.getRakutenLinks();
+
         options.type = 'success';
         this.$toasted.show(res.data.text, options);
       } else {
